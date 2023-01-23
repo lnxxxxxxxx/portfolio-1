9a565ace98e9b546850ccdb2b7ee852a5f7d4c7d
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-01-23 20:12:34 -0300

    subo primeros cambios

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/leandrolun4" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram-alt"></i>
+        </a>
+        
+        <a href="https://www.github.com/lnxxxxxxxx" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/leandro-matias-luna-a572731a5/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
